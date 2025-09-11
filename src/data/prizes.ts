ad1684fc23eede0a0115c0af59d560a81f45e0f3
[dyad] <stbn2023@gmail.com>
2025-09-11 22:57:05 +0200

    Updated src/data/prizes.ts

--- src/data/prizes.ts
+++ src/data/prizes.ts
@@ -9,7 +9,7 @@ export const PRIZES: Prize[] = [
   { id: "prix1", label: "YOURI BIG EYES", image: "/prizes/prix1.png", points: 5 },
   { id: "prix2", label: "OLD YOURI BIKER STYLE", image: "/prizes/prix2.png", points: 10 },
   { id: "prix3", label: "YOURI CLASSICS", image: "/prizes/prix3.png", points: 15 },
-  { id: "prix4", label: "YOURI SUPER SAIYAN", image: "/prizes/prix4.png", points: 20 },
+  { id: "prix4", label: "YOURI SUPER SAIYAN", image: "/prizes/prix4.png", points: 1000 },
   { id: "prix5", label: "YOURI PINKY", image: "/prizes/prix5.png", points: 25 },
   { id: "prix6", label: "YOURI SLUMDOG", image: "/prizes/prix6.png", points: 0 },
   { id: "prix7", label: "YOURI MIGNON", image: "/prizes/prix7.png", points: 30 },
@@ -17,6 +17,6 @@ export const PRIZES: Prize[] = [
   { id: "prix9", label: "YOURI SWAG", image: "/prizes/prix9.png", points: 40 },
   { id: "prix10", label: "THIS IS NOT YOURI", image: "/prizes/prix10.png", points: 8 },
   { id: "prix11", label: "YOURI SANTA DREAM", image: "/prizes/prix11.png", points: 50 },
-  { id: "prix12", label: "YOURI GOLDEN ANGEL", image: "/prizes/prix12.png", points: 18 },
+  { id: "prix12", label: "YOURI GOLDEN ANGEL", image: "/prizes/prix12.png", points: 1500 },
   { id: "prix13", label: "YOURI NO BEBAR", image: "/prizes/prix13.png", points: 35 },
 ];
