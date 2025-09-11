6a15d24037b55873294c1f2203b0149ba433e7d0
[dyad] <stbn2023@gmail.com>
2025-09-11 22:53:37 +0200

    Updated src/data/prizes.ts

--- src/data/prizes.ts
+++ src/data/prizes.ts
@@ -18,5 +18,5 @@ export const PRIZES: Prize[] = [
   { id: "prix10", label: "THIS IS NOT YOURI", image: "/prizes/prix10.png", points: 8 },
   { id: "prix11", label: "OURI SANTA DREAM", image: "/prizes/prix11.png", points: 50 },
   { id: "prix12", label: "YOURI GOLDEN ANGEL", image: "/prizes/prix12.png", points: 18 },
-  { id: "prix13", label: "Prix 13", image: "/prizes/prix13.png", points: 35 },
+  { id: "prix13", label: "YOURI NO BEBAR", image: "/prizes/prix13.png", points: 35 },
 ];
