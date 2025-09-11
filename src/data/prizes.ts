39896611aac22b9a21a27972721cf40e92fa161b
[dyad] <stbn2023@gmail.com>
2025-09-11 22:55:13 +0200

    Updated src/data/prizes.ts

--- src/data/prizes.ts
+++ src/data/prizes.ts
@@ -13,10 +13,10 @@ export const PRIZES: Prize[] = [
   { id: "prix5", label: "YOURI PINKY", image: "/prizes/prix5.png", points: 25 },
   { id: "prix6", label: "YOURI SLUMDOG", image: "/prizes/prix6.png", points: 0 },
   { id: "prix7", label: "YOURI MIGNON", image: "/prizes/prix7.png", points: 30 },
-  { id: "prix8", label: "OURI YELLOWSTONE", image: "/prizes/prix8.png", points: 12 },
+  { id: "prix8", label: "YOURI YELLOWSTONE", image: "/prizes/prix8.png", points: 12 },
   { id: "prix9", label: "YOURI SWAG", image: "/prizes/prix9.png", points: 40 },
   { id: "prix10", label: "THIS IS NOT YOURI", image: "/prizes/prix10.png", points: 8 },
-  { id: "prix11", label: "OURI SANTA DREAM", image: "/prizes/prix11.png", points: 50 },
+  { id: "prix11", label: "YOURI SANTA DREAM", image: "/prizes/prix11.png", points: 50 },
   { id: "prix12", label: "YOURI GOLDEN ANGEL", image: "/prizes/prix12.png", points: 18 },
   { id: "prix13", label: "YOURI NO BEBAR", image: "/prizes/prix13.png", points: 35 },
 ];
